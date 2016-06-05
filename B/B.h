bd825a04733e5aae1e4207f62f07611fde31939d
Ho, Derrick <Dho@webmd.net>
2016-06-04 23:46:37 -0400

    Initial Commit

---
+++ B/B.h
@@ -0,0 +1,19 @@
+//
+//  B.h
+//  B
+//
+//  Created by Ho, Derrick on 6/4/16.
+//  Copyright © 2016 Ho, Derrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for B.
+FOUNDATION_EXPORT double BVersionNumber;
+
+//! Project version string for B.
+FOUNDATION_EXPORT const unsigned char BVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <B/PublicHeader.h>
+
+
